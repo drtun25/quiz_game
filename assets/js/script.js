ea7deeae1f51bba0b5a6786d73cbed21c3a04367
Adedotun O. Albert-Ojeikere <dotun1m@gmail.com>
2021-09-22 22:50:34 -0400

    added a timer but the rest is something else

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,34 +18,116 @@
 //    h1.text('how could click on me like that?!');
 //}
 
-var quizGameEl = $('.quiz-game');
-var quizGameQuestionsEl = $('.quiz-game-questions');
-var gameResultsEl = $('.game-results');
-var quizScoreCounterEl = $('.quiz-score-counter');
-var initialsEl = $('initials');
-var scoreEl = $('.score');
-var resetButtonEl = $('.reset-button');
-var timerEl = $('.timer');
-var timerTextEl = $('.timer-text');
-var timerCountEl = $('.timer-count');
-var startButtonEl = $('.start-button');
+var quizGame = document.querySelector('.quiz-game');
+var quizGameQuestions = document.querySelector('.quiz-game-questions');
+var gameResults = document.querySelector('.game-results');
+var quizScoreCounter = document.querySelector('.quiz-score-counter');
+var initials = document.querySelector('initials');
+var score = document.querySelector('.score');
+var resetButton = document.querySelector('.reset-button');
+var timer = document.querySelector('.timer');
+var timerText = document.querySelector('.timer-text');
+var timerCount = document.querySelector('.timer-count');
+var startButton = document.querySelector('.start-button');
 
+var quizGameQuestions = [];
+
+var quizGame = [ {
+    question: "This is a value that deals with true or false.",
+    option1: "A." + "boolean",
+    option2: "B." + "string",
+    option3: "C." + "function",
+    option4: "D." + "method",
+
+    rightAns: "A."
+},
+
+{
+    question: "This value has something to do with quotations.",
+    option1: "A." + "boolean",
+    option2: "B." + "string",
+    option3: "C." + "function",
+    option4: "D." + "method",
+    
+    rightAns: "B."
+
+},
+
+{
+    question: "This value has a lot to do with numbers.",
+    option1: "A." + "boolean",
+    option2: "B." + "string",
+    option3: "C." + "numbers",
+    option4: "D." + "method",
+
+    rightAns: "C."
+
+},
+
+{
+    question: "This value can be used to make lists.",
+    option1: "A." + "boolean",
+    option2: "B." + "string",
+    option3: "C." + "numbers",
+    option4: "D." + "array",
+    
+    rightAns: "D."
+
+}
+];
+
+
+
+//when the button clicks the game starts
+startButton.addEventListener('click', startGame())
+resetButton.addEventListener('click', startGame())
+
+//the function that starts the game
 function startGame() {
 
+    
+    
+    startTimer()
+    
+
 }
 
+//the function for the timer
+function startTimer() {
+    var time = 10;
+    var timer = setInterval(function() {
+        timerCount.innerHTML='00'+time;
+        time--;
+        if (time < 0) {
+            clearInterval(timer);
+        }
+    }, 1000);
+    
+  
+}
+
+//the function that handles the questions and answers
 function randomQuestions() {
 
-}
 
-function startTimer() {
 
 }
 
+
+
+//the function that displays the score
 function finalScore() {
 
 }
 
-function resetGame() {
-    
+function gameOver () {
+    if ( timer === 0) {
+        window.alert('game over');
+    }
 }
+
+// resetting the game at the end 
+function resetGame() {
+    startGame()
+}
+
