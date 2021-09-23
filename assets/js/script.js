3eaacafd0fc04132e7364592f0d518e718a1c778
Adedotun O. Albert-Ojeikere <dotun1m@gmail.com>
2021-09-22 20:26:13 -0400

    added things to html and javascript

---
+++ assets/js/script.js
@@ -0,0 +1,51 @@
+//var testButton = document.getElementById('test-button');
+//var h1 = document.getElementById('cap');
+
+//testButton.addEventListener('click', buttonClick)
+
+//function buttonClick() {
+//    console.log("you clicked me eww")
+//    h1.textContent = "we both know this is cap!!"
+//}
+
+//var testButtonEl = $('#test-button');
+//var h1 = $('#cap');
+
+//testButtonEl.on('click', buttonClick);
+
+//function buttonClick() {
+//    console.log('how dare u click me');
+//    h1.text('how could click on me like that?!');
+//}
+
+var quizGameEl = $('.quiz-game');
+var quizGameQuestionsEl = $('.quiz-game-questions');
+var gameResultsEl = $('.game-results');
+var quizScoreCounterEl = $('.quiz-score-counter');
+var initialsEl = $('initials');
+var scoreEl = $('.score');
+var resetButtonEl = $('.reset-button');
+var timerEl = $('.timer');
+var timerTextEl = $('.timer-text');
+var timerCountEl = $('.timer-count');
+var startButtonEl = $('.start-button');
+
+function startGame() {
+
+}
+
+function randomQuestions() {
+
+}
+
+function startTimer() {
+
+}
+
+function finalScore() {
+
+}
+
+function resetGame() {
+    
+}
